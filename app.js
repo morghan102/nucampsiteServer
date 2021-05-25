e275c0a879e36915c90d06d1e83109601a9f69ad
Morghan <morghan102@gmail.com>
2021-05-25 16:29:52 -0700

    express sessions part 2

--- app.js
+++ app.js
@@ -53,36 +53,45 @@ app.use(session({
   store: new FileStore() //save sess info to servers hard disk instd of just the running app memory
 }));
 
+//put here so sers can access these routes before authentication
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
 function auth(req, res, next) { //custom middleware. all mw must have req and res as params, next is opt
   console.log(req.session);//sess mw auto adds a session prop to req
   // if (!req.signedCookies.user) { //if cookie isnt signed properly, this property will return false
   if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {//if user hasnt given the authorization
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');//this is what will be sent back to client asking for auth needed
-      err.status = 401;
-      return next(err);//and it is sent here
-    }
-    //buffer is a global node class, dont have to require it bc its global
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');//parses usern and pword into a str from its base64 encoding
-    const user = auth[0];
-    const pass = auth[1];
-    // VERY basic authentication which we will improve l8r
-    if (user === 'admin' && pass === 'password') {
-      // res.cookie('user', 'admin', { signed: true }); //makes new cookie. makes property of 'user' on the sgned cookie obj
-      //'admin' will be stored in name str. 3rd arg is opt - this as true makes the signed cookie
-      req.session.user = 'admin'; //saving to sess that username is admin
-      return next(); // authorized
-    } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    // const authHeader = req.headers.authorization; getting rid of this bc this is getting checked in user.js of models or router or ~
+    // if (!authHeader) {//if user hasnt given the authorization
+    const err = new Error('You are not authenticated!');
+    // res.setHeader('WWW-Authenticate', 'Basic');//this is being done in user router now. this is what will be sent back to client asking for auth needed
+    err.status = 401;
+    return next(err);//and it is sent here
+
+
+// dont need any of this anymore!!!    (    
+  //buffer is a global node class, dont have to require it bc its global
+    // const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');//parses usern and pword into a str from its base64 encoding
+    // const user = auth[0];
+    // const pass = auth[1];
+    // // VERY basic authentication which we will improve l8r
+    // if (user === 'admin' && pass === 'password') {
+    //   // res.cookie('user', 'admin', { signed: true }); //makes new cookie. makes property of 'user' on the sgned cookie obj
+    //   //'admin' will be stored in name str. 3rd arg is opt - this as true makes the signed cookie
+    //   req.session.user = 'admin'; //saving to sess that username is admin
+    //   return next(); // authorized
+    // } else {
+    //   const err = new Error('You are not authenticated!');
+    //   res.setHeader('WWW-Authenticate', 'Basic');
+    //   err.status = 401;
+    //   return next(err);
+    // })
+
+
   } else {
     // if (req.signedCookies.user === 'admin') {
-      if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') { //checks for this val bc thats what we set in user router for successful login
       return next();
     } else {
       const err = new Error('You are not authenticated!');
@@ -95,8 +104,6 @@ function auth(req, res, next) { //custom middleware. all mw must have req and re
 app.use(auth); //we're putting the authentication above this so users cant access static data from the server b4 authentication
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,87 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/signup', (req, res, next) => {
+  User.findOne({username: req.body.username}) //mke sure they arent already a user. f1 returns a prom
+  .then(user => { //either will get a user back or null
+      if (user) {
+          const err = new Error(`User ${req.body.username} already exists!`);
+          err.status = 403;
+          return next(err);
+      } else {
+          User.create({ //prom
+              username: req.body.username,
+              password: req.body.password})
+          .then(user => {
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({status: 'Registration Successful!', user: user});
+          })
+          .catch(err => next(err));
+      }
+  })
+  .catch(err => next(err));// if find1 returns rejected prom
+});
+
+router.post('/login', (req, res, next) => {
+  if(!req.session.user) { //already logged in?
+      const authHeader = req.headers.authorization;
+
+      if (!authHeader) {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+    
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const username = auth[0];
+      const password = auth[1];
+    
+      User.findOne({username: username}) //checks un agst docs in the db. if find mathcing un & p, successful login!
+      .then(user => {
+          if (!user) {
+              const err = new Error(`User ${username} does not exist!`);
+              err.status = 401;
+              return next(err);
+          } else if (user.password !== password) {
+              const err = new Error('Your password is incorrect!');
+              err.status = 401;
+              return next(err);
+          } else if (user.username === username && user.password === password) {
+              req.session.user = 'authenticated';
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'text/plain');
+              res.end('You are authenticated!')
+          }
+      })
+      .catch(err => next(err));
+  } else { //yes logged in
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('You are already authenticated!');
+  }
+});
+
+//logout
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+      req.session.destroy();
+      res.clearCookie('session-id');
+      res.redirect('/');//sends user back to root path
+  } else {
+      const err = new Error('You are not logged in!');
+      err.status = 401;
+      return next(err);
+  }
+});
+
 
 module.exports = router;
